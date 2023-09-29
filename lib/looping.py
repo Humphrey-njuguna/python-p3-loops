0123bedf07e4feff21db3aeeebf4c5f1b33f5980
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-09-29 11:15:31 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -11,3 +11,29 @@ def square_integers(int_list):
 def fizzbuzz():
     # code goes here!
     pass
+def happy_new_year():
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
+
+
+def square_integers(integer_list):
+    squared_list = [x ** 2 for x in integer_list]
+    return squared_list
+
+
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
